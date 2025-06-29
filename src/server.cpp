62d2e0ad46acf970553256c6b533682cca1e6b13
CPPServer <126841556+cppservergit@users.noreply.github.com>
2025-06-29 13:43:16 -0400

    Add files via upload

--- src/server.cpp
+++ src/server.cpp
@@ -70,7 +70,11 @@ server::webapi::webapi(
   roles{std::move(_roles)}, fn{std::move(_fn)}, is_secure{_is_secure} {}
 
 server::server() : m_signal(get_signalfd()), pod_name(get_pod_name()),
-	/* NOSONAR */ server_start_date(std::format("{:%FT%T}", std::chrono::get_tzdb().current_zone()->to_local(std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now()))))
+	/* NOSONAR */ server_start_date(
+						std::format("{:%FT%T}", 
+						std::chrono::get_tzdb().current_zone()
+						->to_local(std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now())))
+						)
 {
 }
 
